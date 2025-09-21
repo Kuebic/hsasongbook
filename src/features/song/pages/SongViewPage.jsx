b43ae3d0354deb1eaa303364ad627d6d28d4c8a8
Kenei Menning <menningkenei@gmail.com>
2025-09-20 21:24:08 -0500

    renamed song-display to song and song-search to search

--- src/features/song/pages/SongViewPage.jsx
+++ src/features/song/pages/SongViewPage.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import songs from '../../shared/data/songs.json'
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
